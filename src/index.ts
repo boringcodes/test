2dbc8013e1df085bbf77c10a3b96a124e25c8283
phatpham9 <phatpham9@gmail.com>
2020-10-21 16:38:54 +0700

    feat: init source

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { MyError } from '@boringcodes/utils/error';
+import errorHandler from '@boringcodes/utils/errorHandler';
+import logger from '@boringcodes/utils/logger';
+
+import config from './config';
+import app from './app';
+
+// start app
+app.listen(config.port, config.host, () => {
+  logger.info(`> App started at http://${config.host}:${config.port}`);
+});
+
+// handle unhandled promise
+process.on('unhandledRejection', (err: MyError) => {
+  throw err;
+});
+
+// handle uncaught error and gracefully shutdown
+process.on('uncaughtException', (err: MyError) => {
+  errorHandler.handle(err);
+});
